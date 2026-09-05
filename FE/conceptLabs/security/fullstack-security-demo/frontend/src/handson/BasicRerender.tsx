65a59f1a71c2b903c897618bbee7def3dcb4c45c
Joy-LJM <2522702541@qq.com>
2026-09-04 22:22:30 -0400

    understanding React rendering

--- FE/conceptLabs/security/fullstack-security-demo/frontend/src/handson/BasicRerender.tsx
+++ FE/conceptLabs/security/fullstack-security-demo/frontend/src/handson/BasicRerender.tsx
@@ -2,18 +2,47 @@ import React from 'react';
 function Counter(){
   const [count, setCount] = React.useState(0);
   console.log('Counter rendered');
+
   return (
     <div>
       <h1>Hello</h1>
       <p>Count: {count}</p>
       <button onClick={() => setCount(count + 1)}>Increment</button>
-      <Child/>
+      <Child1 count={count}/>
+      <Child2 age={1}/>
+      <Child3/>
+      <Child4/>
+      <Child5/>
     </div>
   );
 }
-function Child() {
-  console.log("Child render");
+// React.memo did a shallow prop comparison, saw no props at all (so nothing changed), and skipped the render.
+const Child1 = React.memo(function Child1({count}: {count: number}) {
+  console.log("Child 1 render");
+  return <h2>Hello Child 1, count: {count}</h2>;
+});
+const Child2 = React.memo(function Child2({age}:{age: number}) {
+  console.log("Child 2 render");
+  return <h2>Hello Child 2, age:{age}</h2>;
+});
 
-  return <h2>Hello Child</h2>;
-}
+const Child3 = React.memo(function Child1() {
+  console.log("Child 3 render");
+  return <h2>Hello Child 3</h2>;
+});
+
+// component Child4 is not memoized, so it will re-render every time the parent component re-renders when state changes
+// function Child4() {
+//   console.log("Child 4 render");
+
+//   return <h2>Hello Child 4</h2>;
+// }
+const Child4 = React.memo(function Child4() {
+  console.log("Child 4 render");
+  return <h2>Hello Child 4</h2>;
+});
+const Child5 = React.memo(function Child5() {
+  console.log("Child 5 render");
+  return <h2>Hello Child 5</h2>;
+});
 export default Counter
