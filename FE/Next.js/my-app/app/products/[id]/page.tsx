72c57fa11b60ab28b586e5d5359b6bc31e23cd12
Joy-LJM <2522702541@qq.com>
2026-09-19 22:44:14 -0400

    next.js metadata and image optimization

--- FE/Next.js/my-app/app/products/[id]/page.tsx
+++ FE/Next.js/my-app/app/products/[id]/page.tsx
@@ -1,7 +1,28 @@
-'use client'
+import type { Metadata } from "next";
+import Image from "next/image";
 
-export default async function ProductDetailPage({params}:{params:Promise<{id:string}>}){
-  // Next.js 16, dynamic route props are async
-  const {id} = await params;
-  return <div>product detail page of {await id}</div>
+type ProductDetailPageProps = {
+  params: Promise<{ id: string }>;
+};
+
+export async function generateMetadata({
+  params,
+}: ProductDetailPageProps): Promise<Metadata> {
+  const { id } = await params;
+
+  return {
+    title: `Product ${id}`,
+  };
+}
+
+export default async function ProductDetailPage({
+  params,
+}: ProductDetailPageProps) {
+  const { id } = await params;
+
+  return <div>product detail page of {id}
+  {/* <img src="https://wimg.mk.co.kr/news/cms/202603/11/news-p.v1.20260311.9acbf71fa9c5490daded6f07793a3c88_P1.jpg" alt="image" width={500} height={500}/> */}
+  {/* remote image need to set width and height */}
+  <Image src="https://wimg.mk.co.kr/news/cms/202603/11/news-p.v1.20260311.9acbf71fa9c5490daded6f07793a3c88_P1.jpg" alt="image" width={500} height={500}/>
+  </div>;
 }
--- FE/Next.js/my-app/next.config.ts
+++ FE/Next.js/my-app/next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "wimg.mk.co.kr",
+        port: "",
+        pathname: "/news/cms/202603/11/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
