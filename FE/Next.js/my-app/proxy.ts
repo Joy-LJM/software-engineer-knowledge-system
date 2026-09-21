f4affdd016a9d10a7e85fe5482b0af593c6cb733
Joy-LJM <2522702541@qq.com>
2026-09-20 20:14:56 -0400

    next.js proxy

--- FE/Next.js/my-app/proxy.ts
+++ FE/Next.js/my-app/proxy.ts
@@ -4,8 +4,8 @@ import type { NextRequest } from "next/server";
 export function proxy(request: NextRequest) {
   return NextResponse.redirect(new URL("/", request.url));
 }
-
 // specify paths where the Proxy applies, in this case all paths under /about/*
+// the status code for the redirect is 307 (Temporary Redirect) by default, but you can specify a different status code if needed
 export const config = {
   matcher: "/about/:path*",
 };
